fa29351270f9b644c7c7958919c2037e5413b28a
ProgramLikeABeast <xuyang000111@gmail.com>
2024-03-07 15:58:23 -0500

    temporarily commented out some bug files

--- src/main/java/com/example/springbootrest/rest/LogInController.java
+++ src/main/java/com/example/springbootrest/rest/LogInController.java
@@ -1,19 +1,19 @@
-package com.example.springbootrest.rest;
-
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/user_profile")
-public class LogInController {
-    @GetMapping
-    public OAuth2User user(@AuthenticationPrincipal OAuth2User principal) {
-        return principal;
-    }
-}
+//package com.example.springbootrest.rest;
+//
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Collections;
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/user_profile")
+//public class LogInController {
+//    @GetMapping
+//    public OAuth2User user(@AuthenticationPrincipal OAuth2User principal) {
+//        return principal;
+//    }
+//}
