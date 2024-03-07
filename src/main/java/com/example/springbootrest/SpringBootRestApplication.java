a72b6b096b8a318e170f8dac48d537fd8e325d61
ProgramLikeABeast <xuyang000111@gmail.com>
2024-03-07 17:11:15 -0500

    disabled spring security for testing

--- src/main/java/com/example/springbootrest/SpringBootRestApplication.java
+++ src/main/java/com/example/springbootrest/SpringBootRestApplication.java
@@ -2,10 +2,10 @@ package com.example.springbootrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringBootRestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApplication.class, args);
 	}
